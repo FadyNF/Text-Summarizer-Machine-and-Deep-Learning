a8fcdacc33abc93c7a7b88d4b607a90dc224fff0
FadyNF <fadynf05@gmail.com>
2025-05-18 16:12:29 +0300

    Add NLTK resource download for English POS tagging and create requirements file

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -11,6 +11,8 @@ nltk.download('punkt', quiet=True)
 nltk.download('stopwords', quiet=True)
 nltk.download('wordnet', quiet=True)
 nltk.download('averaged_perceptron_tagger', quiet=True)
+nltk.download('averaged_perceptron_tagger_eng', quiet=True)
+
 
 # ========== TEXT PROCESSING FUNCTIONS ==========
 
